31a05164d2857862a43fc8ba4dc9a7ebe5091850
vivek-1354 <108834876+vivek-1354@users.noreply.github.com>
2022-07-17 14:16:08 +0530

    Update store.py

--- models/store.py
+++ models/store.py
@@ -1,20 +1,18 @@
 from db import db
 
-from model.item import ItemModel
-
 class StoreModel(db.Model):
     __tablename__ ='stores'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     
-    db.relationship(ItemModel)
+    items = db.relationship('ItemModel', lazy = 'dynamic')
     
     def __init__(self,name):
         self.name = name
 
     def json(self):
-        return {"id":self.id,"name":self.name}
+        return {"name":self.name,"items":[item.json() for item in self.items.all()]}
 
         # This function authorization if item name exits in database data.sqlite if not exits return none
     @classmethod
